68b4dea22c6205de47e39bbd4ff8712ae837ad1b
dianmaruf <dianmaruf1997@gmail.com>
2017-10-12 18:41:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+ <html>
+	<head>
+		<title>Online PHP Script Execution</title>
+	</head>
+	
+	<body>
+	
+	<?php
+		echo "My first PHP script!";
+	class MyClass
+	{
+	public $prop1 = "i'm a class property";
+	}
+	class YourClass
+	{
+	public $prop1 = "you are a class property";
+	}
+
+	$obj = new MyClass;
+	$obj2 = new YourClass;
+
+	var_dump($obj, $obj2);
+	var_dump($obj2);
+	
+	?>
+
+	</body>
+</html> # HelloCUK
+# HelloCUK
+# HelloCUK2
